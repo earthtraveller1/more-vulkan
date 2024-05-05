53a3aff0ca698ade30e11116da05d7dd514af989
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-05-05 16:55:16 -0400

    Created the Vulkan instance.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string_view>
 
+#include <vulkan/vulkan.h>
+
 namespace {
 struct glfw_init_failed_exception : public std::exception {
     virtual const char *what() const noexcept override {
@@ -14,6 +16,16 @@ struct glfw_init_failed_exception : public std::exception {
 struct glfw_window_creation_failed_exception : public std::exception {
     virtual const char *what() const noexcept override {
         return "Failed to create the GLFW window.";
+    }
+};
+
+struct vulkan_exception : public std::exception {
+    VkResult error_code;
+
+    vulkan_exception(VkResult p_error_code) : error_code(p_error_code) {}
+
+    virtual const char *what() const noexcept override {
+        return "A Vulkan error occured.";
     }
 };
 
@@ -38,10 +50,42 @@ auto create_window(std::string_view p_title, int p_width, int p_height)
 
     return window;
 }
+
+auto create_vulkan_instance() -> VkInstance {
+    const VkApplicationInfo application_info{
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Stupid Vulkan App",
+        .apiVersion = VK_API_VERSION_1_2,
+    };
+
+    uint32_t glfw_extension_count;
+    const auto glfw_extensions =
+        glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+    const VkInstanceCreateInfo instance_create_info{
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext = nullptr,
+        .pApplicationInfo = &application_info,
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = nullptr,
+        .enabledExtensionCount = glfw_extension_count,
+        .ppEnabledExtensionNames = glfw_extensions,
+    };
+
+    VkInstance instance;
+    const auto result =
+        vkCreateInstance(&instance_create_info, nullptr, &instance);
+    if (result != VK_SUCCESS) {
+        throw vulkan_exception{result};
+    }
+
+    return instance;
+}
 } // namespace
 
 int main() {
     try {
+        const auto instance = create_vulkan_instance();
         const auto window = create_window("Hello!", 1280, 720);
 
         glfwShowWindow(window);
@@ -51,6 +95,11 @@ int main() {
 
         glfwDestroyWindow(window);
         glfwTerminate();
+
+        vkDestroyInstance(instance, nullptr);
+    } catch (const vulkan_exception &e) {
+        std::cerr << "[ERROR]: Vulkan error " << e.error_code << '\n';
+        return EXIT_FAILURE;
     } catch (const std::exception &e) {
         std::cerr << "[ERROR]: " << e.what() << '\n';
         return EXIT_FAILURE;
