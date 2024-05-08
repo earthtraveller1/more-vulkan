bff88de5248d843139a2bfbc355691e8ecc9681e
earthtraveller1 <liangyuezhao7@gmail.com>
2024-05-08 17:36:53 -0400

    Used a function level try-catch.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include "graphics.hpp"
 #include "device.hpp"
 #include "enumerate.hpp"
 #include "errors.hpp"
+#include "graphics.hpp"
 #include "present.hpp"
 
-int main(int p_argc, const char *const *const p_argv) {
+int main(int p_argc, const char *const *const p_argv) try {
     bool enable_validation = false;
 
     for (auto arg = p_argv; arg < p_argv + p_argc; ++arg) {
@@ -22,28 +22,23 @@ int main(int p_argc, const char *const *const p_argv) {
         throw mv::glfw_init_failed_exception{};
     }
 
-    try {
-        const auto instance = mv::vulkan_instance_t::create(enable_validation);
-        const auto window = mv::window_t::create(instance, "Hello!", 1280, 720);
-        const auto device =
-            mv::vulkan_device_t::create(instance, window.surface);
-        const auto swapchain = mv::swapchain_t::create(device, window);
-        const auto render_pass = mv::render_pass_t::create(device, swapchain);
-        const auto pipeline = mv::graphics_pipeline_t::create(
-            device, render_pass, "shaders/basic.vert.spv",
-            "shaders/basic.frag.spv"
-        );
+    const auto instance = mv::vulkan_instance_t::create(enable_validation);
+    const auto window = mv::window_t::create(instance, "Hello!", 1280, 720);
+    const auto device = mv::vulkan_device_t::create(instance, window.surface);
+    const auto swapchain = mv::swapchain_t::create(device, window);
+    const auto render_pass = mv::render_pass_t::create(device, swapchain);
+    const auto pipeline = mv::graphics_pipeline_t::create(
+        device, render_pass, "shaders/basic.vert.spv", "shaders/basic.frag.spv"
+    );
 
-        glfwShowWindow(window.window);
-        while (!glfwWindowShouldClose(window.window)) {
-            glfwPollEvents();
-        }
-    } catch (const mv::vulkan_exception &e) {
-        std::cerr << "[ERROR]: Vulkan error " << e.error_code << '\n';
-        return EXIT_FAILURE;
-    } catch (const std::exception &e) {
-        std::cerr << "[ERROR]: " << e.what() << '\n';
-        return EXIT_FAILURE;
+    glfwShowWindow(window.window);
+    while (!glfwWindowShouldClose(window.window)) {
+        glfwPollEvents();
     }
+} catch (const mv::vulkan_exception &e) {
+    std::cerr << "[ERROR]: Vulkan error " << e.error_code << '\n';
+    return EXIT_FAILURE;
+} catch (const std::exception &e) {
+    std::cerr << "[ERROR]: " << e.what() << '\n';
+    return EXIT_FAILURE;
 }
-
