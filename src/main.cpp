99d6e006d52c6ffbde5b23f68e683259da23263a
earthtraveller1 <liangyuezhao7@gmail.com>
2024-05-08 16:04:50 -0400

    Finished specifying all the fixed functions part of the pipeline.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -30,6 +31,30 @@ struct graphics_pipeline_t {
         vkDestroyPipeline(device.logical, pipeline, nullptr);
     }
 };
+
+struct vector_3f_t {
+    float x;
+    float y;
+    float z;
+};
+
+struct vertex_t {
+    vector_3f_t position;
+};
+
+constexpr VkVertexInputBindingDescription vertex_input_binding_description{
+    .binding = 0,
+    .stride = sizeof(vertex_t),
+    .inputRate = VK_VERTEX_INPUT_RATE_VERTEX,
+};
+
+constexpr static std::array<VkVertexInputAttributeDescription, 1>
+    vertex_attribute_descriptions{VkVertexInputAttributeDescription{
+        .location = 0,
+        .binding = 0,
+        .format = VK_FORMAT_R32G32B32_SFLOAT,
+        .offset = offsetof(vertex_t, position),
+    }};
 
 auto read_file_to_vector(std::string_view file_name) -> std::vector<char>;
 } // namespace
@@ -108,7 +133,7 @@ auto graphics_pipeline_t::create(
         throw mv::vulkan_exception{result};
     }
 
-    const std::array<VkPipelineShaderStageCreateInfo, 2> shader_stages {
+    const std::array<VkPipelineShaderStageCreateInfo, 2> shader_stages{
         VkPipelineShaderStageCreateInfo{
             .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
             .stage = VK_SHADER_STAGE_VERTEX_BIT,
@@ -123,10 +148,69 @@ auto graphics_pipeline_t::create(
         },
     };
 
+    const VkPipelineVertexInputStateCreateInfo vertex_input_state_create_info{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        .vertexBindingDescriptionCount = 1,
+        .pVertexBindingDescriptions = &vertex_input_binding_description,
+        .vertexAttributeDescriptionCount = vertex_attribute_descriptions.size(),
+        .pVertexAttributeDescriptions = vertex_attribute_descriptions.data(),
+    };
+
+    const VkPipelineInputAssemblyStateCreateInfo input_assembly_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+        .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+        .primitiveRestartEnable = VK_FALSE,
+    };
+
+    const VkPipelineViewportStateCreateInfo viewport_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+        .viewportCount = 1,
+        .scissorCount = 1,
+    };
+
+    const VkPipelineRasterizationStateCreateInfo rasterization_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .depthClampEnable = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode = VK_POLYGON_MODE_FILL,
+        .cullMode = VK_CULL_MODE_BACK_BIT,
+        .frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
+        .depthBiasEnable = VK_FALSE,
+        .lineWidth = 1.0f,
+    };
+
+    const VkPipelineMultisampleStateCreateInfo multisample_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+        .sampleShadingEnable = VK_FALSE,
+    };
+
+    const VkPipelineColorBlendAttachmentState color_blend_attachment{
+        .blendEnable = VK_FALSE,
+        .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+                          VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
+    };
+
+    const VkPipelineColorBlendStateCreateInfo color_blend_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        .logicOpEnable = VK_FALSE,
+        .logicOp = VK_LOGIC_OP_COPY,
+        .attachmentCount = 1,
+        .pAttachments = &color_blend_attachment,
+    };
+
     const VkGraphicsPipelineCreateInfo pipeline_create_info{
         .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
         .stageCount = static_cast<uint32_t>(shader_stages.size()),
         .pStages = shader_stages.data(),
+        .pVertexInputState = &vertex_input_state_create_info,
+        .pInputAssemblyState = &input_assembly_state,
+        .pTessellationState = nullptr,
+        .pViewportState = &viewport_state,
+        .pRasterizationState = &rasterization_state,
+        .pMultisampleState = &multisample_state,
+        .pDepthStencilState = nullptr,
+        .pColorBlendState = &color_blend_state,
     };
 
     VkPipeline pipeline;
