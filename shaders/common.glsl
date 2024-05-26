73dd7adcae332e2df98b27a63d8be9dc690bac7a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-05-26 14:19:43 -0400

    Established a GLSL header for the shaders.

---
+++ shaders/common.glsl
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_common_glsl
+#define INCLUDED_common_glsl
+
+layout (binding = 0) uniform uniform_buffer_object_t {
+    mat4 projection;
+    mat4 view;
+    mat4 model;
+    vec3 light_position;
+} uniform_buffer_object;
+
+#endif
+
