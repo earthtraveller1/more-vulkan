5d137bfeabbc1e4cd9f7fd29466f53db477d014a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-05-27 17:29:13 -0400

    Added a simple memory allocation abstraction.

--- src/device.hpp
+++ src/device.hpp
@@ -2,6 +2,8 @@
 
 #include <vulkan/vulkan.h>
 
+#include "common.hpp"
+
 namespace mv {
 struct vulkan_instance_t {
     VkInstance instance;
@@ -34,6 +36,44 @@ struct vulkan_device_t {
 
     inline ~vulkan_device_t() {
         vkDestroyDevice(logical, nullptr);
+    }
+};
+} // namespace mv
+
+#include "images.hpp"
+
+namespace mv {
+
+struct vulkan_memory_t {
+    VkDeviceMemory memory;
+    VkDeviceSize size;
+    VkDeviceSize bind_offset{0};
+
+    const vulkan_device_t &device;
+
+    vulkan_memory_t(
+        VkDeviceMemory p_memory,
+        VkDeviceSize p_size,
+        const vulkan_device_t &p_device
+    )
+        : memory(p_memory), size(p_size), device(p_device) {}
+
+    static auto allocate(
+        const vulkan_device_t &device,
+        std::span<const VkMemoryRequirements> requirements,
+        VkMemoryPropertyFlags property_flags
+    ) -> vulkan_memory_t;
+
+    inline auto bind_image(const vulkan_texture_t &image, VkDeviceSize size)
+        -> void {
+        vkBindImageMemory(device.logical, image.image, memory, bind_offset + size);
+        bind_offset += size;
+    }
+
+    NO_COPY(vulkan_memory_t);
+
+    inline ~vulkan_memory_t() {
+        vkFreeMemory(device.logical, memory, nullptr);
     }
 };
 
